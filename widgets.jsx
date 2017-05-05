4ff88256d1b8910365948ffaa3c2028c1cdfae7f
Tu Ngo <tungo8810@gmail.com>
2017-05-05 11:43:14 -0700

    Create entry file

---
+++ widgets.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const main = document.getElementById('main');
+  ReactDOM.render(<Root/>, main);
+});
